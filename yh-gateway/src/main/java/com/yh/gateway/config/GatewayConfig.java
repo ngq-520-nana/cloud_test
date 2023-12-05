d05cc264815c6cdfd7aa4b3c004bb906575678ed
niuguoqiang <421336380@qq.com>
2023-12-05 15:55:23 +0800

    first

---
+++ yh-gateway/src/main/java/com/yh/gateway/config/GatewayConfig.java
@@ -0,0 +1,23 @@
+package com.yh.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import com.yh.gateway.handler.SentinelFallbackHandler;
+
+/**
+ * 网关限流配置
+ *
+ * @author ruoyi
+ */
+@Configuration
+public class GatewayConfig
+{
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelFallbackHandler sentinelGatewayExceptionHandler()
+    {
+        return new SentinelFallbackHandler();
+    }
+}
